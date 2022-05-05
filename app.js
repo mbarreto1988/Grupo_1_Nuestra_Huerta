0966680b00cab0a1692b65b20d648036f7416f81
mbarreto1988 <barretomatiasgabriel@gmail.com>
2022-05-04 21:03:11 -0300

    agregando paquetes express y nodemon

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-Hola
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/inicio.html');
+});
+
+
+// npm start
