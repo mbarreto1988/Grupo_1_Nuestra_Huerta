97e93e470418b6722da26b8c1d842c0fd7079882
Juan Pablo Scrigna <JPScrigna@aguas.dom>
2022-05-05 11:45:24 -0300

    Inicio Codeo de Register.html

--- app.js
+++ app.js
@@ -11,5 +11,12 @@ app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/inicio.html');
 });
 
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
 
 // npm start
