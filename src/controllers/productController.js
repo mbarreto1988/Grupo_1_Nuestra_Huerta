b6011c511ff99a51baa5bb6049d8fbeeb59244db
mbarreto1988 <barretomatiasgabriel@gmail.com>
2022-05-30 15:40:34 -0300

    enrutando

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -5,6 +5,27 @@ const controller = {
     bananas: (req,res) => {
         res.render('bananas');
     },
+    kiwi: (req,res) => {
+        res.render('kiwi');
+    },
+    brocoli: (req,res) => {
+        res.render('brocoli');
+    },
+    champignon: (req,res) => {
+        res.render('champignon');
+    },
+    lechuga: (req,res) => {
+        res.render('lechuga');
+    },
+    limon: (req,res) => {
+        res.render('limon');
+    },
+    mandarina: (req,res) => {
+        res.render('mandarina');
+    },
+    papa: (req,res) => {
+        res.render('papa');
+    },
     frutasOrganicas: (req, res) => {
         res.render('frutasOrganicas');
     },
--- src/routers/productRoutes.js
+++ src/routers/productRoutes.js
@@ -3,7 +3,14 @@ const productController = require('../controllers/productController');
 const router = express.Router();
 
 router.get('/create', productController.formProd);
-router.get('/detalleProducto', productController.bananas);
+router.get('/detalleProducto_bananas', productController.bananas);
+router.get('/detalleProducto_kiwi', productController.kiwi);
+router.get('/detalleProducto_brocoli', productController.brocoli);
+router.get('/detalleProducto_champignon', productController.champignon);
+router.get('/detalleProducto_lechuga', productController.lechuga);
+router.get('/detalleProducto_limon', productController.limon);
+router.get('/detalleProducto_mandarina', productController.mandarina);
+router.get('/detalleProducto_papa', productController.papa);
 router.get('/frutasOrganicas', productController.frutasOrganicas);
 router.get('/verdurasOrganicas', productController.verdurasOrganicas);
 router.get('/bolsonesYPromos', productController.bolsonesYPromos);
