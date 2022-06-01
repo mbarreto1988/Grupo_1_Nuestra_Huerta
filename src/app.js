b3454494e47397e6a92355c1078687f4e199c52e
JuampiScrigna <jpscae@hotmail.com>
2022-06-01 11:20:58 -0300

    agregado error 404

--- src/app.js
+++ src/app.js
@@ -26,6 +26,9 @@ app.use('/', mainRouter);
 app.use('/product', productRouter);
 app.use('/user', userRouter);
 
+// ************ catch 404 and forward to error handler ************
+app.use((req, res, next) => { res.status(404).render('not-found'); })
+
 app.listen(3000, () => { console.log('Servidor arriba en el puerto 3000 👌');}) //aca se crea la direccion del localHost
 
 // const inicioRouter = require("./routers/inicio");
