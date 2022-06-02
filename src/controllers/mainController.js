4b3770f46462e35130de364e7ad975306ee2a195
Matias <barretomatiasgabriel@gmail.com>
2022-06-01 23:34:22 -0300

    agregando unos datos

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -4,15 +4,15 @@ const fileProducts = fs.readFileSync(path.resolve("src/data/productos.json"), "u
 
 const products = JSON.parse(fileProducts);
 
-//const masVisitados = products.filter(element => element.seccion == "ppp")
-
-const masVisitados = products.filter(element => element.descuento > 15)
+const destacadosMes = products.filter(element => element.seccion == "destacados del mes");
+// const masVisitados = products.filter(element => element.seccion == "combos");
+const ofertas = products.filter(element => element.descuento > 15)
 
 //busca el nombre del ejs a renderizar
 const controller = {
     inicio: (req, res) => {
         console.log(products)
-        res.render('inicio', {products: masVisitados}); // "inicio" EJS a renderizar {products} la keyword
+        res.render('inicio', {products: destacadosMes, ofertas}); // "inicio" EJS a renderizar {products} la keyword
     },
     
 
--- src/routers/productRoutes.js
+++ src/routers/productRoutes.js
@@ -22,17 +22,17 @@ router.get('/product-create-form', productController.create);
 router.post('/create', upload.single("image"), productController.store); 
 
 
-router.get('/detalleProducto_bananas', productController.bananas);
-router.get('/detalleProducto_kiwi', productController.kiwi);
-router.get('/detalleProducto_brocoli', productController.brocoli);
-router.get('/detalleProducto_champignon', productController.champignon);
-router.get('/detalleProducto_lechuga', productController.lechuga);
-router.get('/detalleProducto_limon', productController.limon);
-router.get('/detalleProducto_mandarina', productController.mandarina);
-router.get('/detalleProducto_papa', productController.papa);
-router.get('/frutasOrganicas', productController.frutasOrganicas);
-router.get('/verdurasOrganicas', productController.verdurasOrganicas);
-router.get('/bolsonesYPromos', productController.bolsonesYPromos);
-router.get('/nuestrasRecetas', productController.nuestrasRecetas);
+// router.get('/detalleProducto_bananas', productController.bananas);
+// router.get('/detalleProducto_kiwi', productController.kiwi);
+// router.get('/detalleProducto_brocoli', productController.brocoli);
+// router.get('/detalleProducto_champignon', productController.champignon);
+// router.get('/detalleProducto_lechuga', productController.lechuga);
+// router.get('/detalleProducto_limon', productController.limon);
+// router.get('/detalleProducto_mandarina', productController.mandarina);
+// router.get('/detalleProducto_papa', productController.papa);
+// router.get('/frutasOrganicas', productController.frutasOrganicas);
+// router.get('/verdurasOrganicas', productController.verdurasOrganicas);
+// router.get('/bolsonesYPromos', productController.bolsonesYPromos);
+// router.get('/nuestrasRecetas', productController.nuestrasRecetas);
 
 module.exports = router;
