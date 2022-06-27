7982e461d3a901707f0798300f626749b52165a6
JuampiScrigna <jpscae@hotmail.com>
2022-06-27 16:16:05 -0300

    cambios profile y products

--- src/routers/adminRoutes.js
+++ src/routers/adminRoutes.js
@@ -10,7 +10,7 @@ const adminController = require(path.resolve(__dirname,'../controllers/adminCont
 //Como podemos indicar para subir el archivo nombre y donde guardarlo
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, path.resolve(__dirname, '../../public/images/productos'));
+      cb(null, path.resolve(__dirname, '../../public/img/producto'));
     },
     filename: function (req, file, cb) {
       cb(null, 'producto-'+Date.now()+path.extname(file.originalname))
@@ -25,13 +25,13 @@ router.get('/administrar', adminController.index);
 // router.post('/administrar/create', upload.single('image'), adminController.save);
 router.get('/administrar/detail/:id', adminController.show);
 router.get('/administrar/edit/:id', adminController.edit);
-router.put('/administrar/edit/:id', upload.single('image'), adminController.update);
+router.put('/administrar/edit/:id', upload.single('imagen'), adminController.update);
 router.delete('/administrar/delete/:id', adminController.destroy);
 
 
 /*** CREATE ONE PRODUCT ***/ 
 router.get('/administrar/create', adminController.create); 
-router.post('/administrar/create', upload.single("image"), adminController.store); 
+router.post('/administrar/create', upload.single("imagen"), adminController.store); 
 
 
 module.exports = router;
